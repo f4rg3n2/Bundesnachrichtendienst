c33836fc3c96ebf6700ee7c0ea7a04e707f62372
Jens <Jens@Jens-PC>
2014-12-15 20:40:56 +0100

    Schreibfehler verbessert

--- Bundesnachrichtendienst/src/de/hs_ma/tpews14/ib9/Crypter/IllegalKeyException.java
+++ Bundesnachrichtendienst/src/de/hs_ma/tpews14/ib9/Crypter/IllegalKeyException.java
@@ -3,7 +3,7 @@ package de.hs_ma.tpews14.ib9.Crypter;
 public class IllegalKeyException extends CrypterException{
 
 	public IllegalKeyException() {
-	    super("Schlüssel ist nihct gültig.");
+	    super("Schlüssel ist nicht gültig.");
     }
 	public IllegalKeyException(String message){
 		super(message);
